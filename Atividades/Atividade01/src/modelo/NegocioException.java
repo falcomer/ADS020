3fa23b376a74a1f160ccf673b8e308b793993267
18214290014 <18214290014@OLAB04-D05.academico.iesb.br>
2020-03-10 19:38:53 -0300

    Atividade04

---
+++ Atividades/Atividade01/src/modelo/NegocioException.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class NegocioException extends Exception{
+
+    public NegocioException(String string) {
+        super(string);
+    }
+
+    public NegocioException(String string, Throwable thrwbl) {
+        super(string, thrwbl);
+    }
+    
+}
